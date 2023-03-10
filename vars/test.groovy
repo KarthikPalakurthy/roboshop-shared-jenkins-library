8d56b22995aa4525f0c1081c37b2363434ab2eb4
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-03-10 12:50:54 +0000

    jenkins test

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,3 +1,32 @@
-def call (name = 'fruit_name') {
- echo ' Hello ${name}'
+def abc = "10"
+println abc
+
+println "If condition loop"
+
+if (abc=="10"){
+ println "Yes"
+}else{
+ println "No"
+}
+
+println "While loop"
+
+def a = 7
+def b = 2
+while(a > b){
+ println "${b}"
+ b++
+}
+
+println "For loop"
+
+for (int i =0; i<=5; i++){
+ println "${i}"
+}
+
+println "Defining an array"
+
+def fruits = ["apple","mango", "orange", "pineapple"]
+for (i in fruits){
+ println(i);
 }

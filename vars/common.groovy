8c2c05711654b9c1ea4e9b019e604a9b34be08e6
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-03-11 19:49:48 +0000

    jenkins test

--- vars/common.groovy
+++ vars/common.groovy
@@ -24,5 +24,5 @@ def unittests() {
 
 
 def email(email_note) {
-    println email_note
+    mail bcc: '', body: 'TEST', cc: '', from: '', replyTo: '', subject: 'Failure of Job ', to: 'palakurthyk95@gmail.com'
 }

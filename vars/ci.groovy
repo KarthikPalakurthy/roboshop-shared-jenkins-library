c894fd79d8d58772d8b3b2e3c302e44adb9677da
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-03-19 21:26:48 +0000

    jenkins test

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -19,9 +19,17 @@ def call() {
                     }
                 }
                 stage('Quality Control') {
+                    environment {
+                        SONAR_USER= '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user --query Parameters[0].Value --with-decryption| sed \\\'s/"//g\\\')'
+                        SONAR_PASS= '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass --query Parameters[0].Value --with-decryption| sed \\\'s/"//g\\\')'
+
+                    }
                     steps {
                         script {
-                            common.unittests()
+                            withAWSParameterStore(credentialsId: '', naming: 'relative', path: '/service', recursive: true, regionName: 'us-east-1') {
+                                echo ' Hello '
+                                }
+                            sh 'sonar-scanner -Dsonar.host.url=http://172.31.2.143:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart'
                         }
                     }
                 }

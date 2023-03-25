008228901572d1c566b259b4e283f3e3687fbebd
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-03-25 21:04:20 +0000

    jenkins test

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,19 +1,15 @@
 def call() {
     try {
-        node('test-work') {
-
+        node ('test-work') {
             stage('Cleanup') {
                 cleanWs()
             }
-
             stage('Compile/build') {
                 common.compile()
             }
-
             stage('Unit Tests') {
                 common.unittests()
             }
-
             stage('Quality Control') {
                 SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
                 SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
